830793d94a1c4bd8e525d50debad18376dfd0a4f
isaacrdz <zack_cpp@hotmail.com>
2021-02-26 12:54:03 -0600

    s

--- pages/categorias/[categoria].js
+++ pages/categorias/[categoria].js
@@ -1,14 +1,62 @@
 import React from "react";
 import { useRouter } from "next/router";
+import CarList from "../../components/autos/CarList";
+import Meta from "../../components/Meta";
+import { Container, Divider } from "@material-ui/core";
 
-const categoria = () => {
+const categoria = ({ vehicles }) => {
   const router = useRouter();
+  const { categoria } = router.query;
 
-  React.useEffect(() => {
-    console.log(router);
-  }, []);
+  return (
+    <>
+      <Meta
+        title={`Autos tipo ${categoria.toUpperCase()}`}
+        description={`Busca tu auto tipo ${categoria}`}
+      />
 
-  return <div>Aqui va la categoria</div>;
+      <Container maxWidth="lg">
+        <Divider style={{ marginBottom: "10px" }} />
+
+        <Divider style={{ marginBottom: "50px" }} />
+
+        <CarList vehicles={vehicles} />
+      </Container>
+    </>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://c1-app-api.herokuapp.com/api/v1/vehicles?modelType=${context.params.categoria}`
+  );
+
+  const vehicles = await res.json();
+
+  return {
+    props: {
+      vehicles: vehicles.data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://c1-app-api.herokuapp.com/api/v1/vehicles`);
+
+  const vehiclesData = await res.json();
+
+  const vehicles = vehiclesData.data;
+
+  const categories = vehicles.map((vehicle) => vehicle.modelType);
+
+  const paths = categories.map((category) => ({
+    params: { categoria: category.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export default categoria;
